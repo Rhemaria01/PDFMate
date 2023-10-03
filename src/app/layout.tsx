bc097d06f9f3eb3843aa32ed50f62475f90cd89f
Reenav Hemaria <git@SJCEM23>
2023-10-03 09:57:50 +0530

    Pdf Renderer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,10 @@ import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import Providers from '@/components/Providers'
 
+import "react-loading-skeleton/dist/skeleton.css"
+import 'simplebar-react/dist/simplebar.min.css'
+
+import { Toaster } from '@/components/ui/toaster'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -21,9 +25,9 @@ export default function RootLayout({
     <html lang="en" className='light'>
       <Providers>
       <body 
-      className={cn('min-h-screen font-sans antialiased ',
+      className={cn('min-h-screen grainy font-sans antialiased ',
       inter.className)}>
-
+      <Toaster />
        <Navbar />
         {children}
 
