c0f7a26ad77639aa25dd4320f0edf34d6a4e0508
Reenav Hemaria <git@SJCEM23>
2023-10-04 15:00:05 +0530

    Stripe Webhook

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import Link from "next/link"
 import { buttonVariants } from './ui/button'
-import {LoginLink, RegisterLink} from "@kinde-oss/kinde-auth-nextjs/server"
+import {LoginLink, RegisterLink, getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server"
 import { ArrowRight } from 'lucide-react'
+import UserAccountNav from './UserAccountNav'
 const Navbar = () => {
+    const {getUser} = getKindeServerSession()
+    const user = getUser()
   return (
     <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
         <MaxWidthWrapper>
@@ -13,7 +16,7 @@ const Navbar = () => {
                    <span> PDFMate</span>
                 </Link>
                 <div className='hidden items-center space-x-4 sm:flex'>
-                    <>
+                    {!user ?<>
                         <Link href="/pricing" className={buttonVariants({
                             variant: "ghost",
                             size: "sm"
@@ -29,7 +32,18 @@ const Navbar = () => {
                         })}>
                             Get Started <ArrowRight className='ml-1.5 h-5 w-5'/>
                         </RegisterLink>
-                    </>
+                    </> : <>
+                        <Link href="/dashboard" className={buttonVariants({
+                            variant: "ghost",
+                            size: "sm"
+                        })}>Dashboard
+                        </Link>
+                        <UserAccountNav email={user.email ?? ''} 
+                        name={!user.given_name || 
+                            !user.family_name ? 
+                            "Your Account" : `${user.given_name} ${user.family_name}`}
+                        imageUrl={user.picture ?? ''} />
+                    </>}
                 </div>
             </div>
         </MaxWidthWrapper>
